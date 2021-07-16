381c05556094b22377e36caeb6e3e4db5b2926ae
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-07-16 13:32:53 +0100

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -1,4 +1,5 @@
 #python -m pip install pandas --user
+import zipfile
 import pandas as pd
 print("Testing basic print function")
 print("wanted to check if the update actuallly worked")
@@ -9,4 +10,10 @@ t = pd.read_csv("test_dataset.csv")
 for i in t:
     print(i)
     
+    
+with zipfile.ZipFile("testzip.zip", 'r') as zip_ref:
+    zip_ref.extractall()
+    
+    
+
 
