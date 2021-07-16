5fd635958723621baac374622a6f28e486351e60
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-07-16 11:21:03 +0100

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -1,7 +1,7 @@
 python -m pip install pandas --user
 import pandas as pd
 print("Testing basic print function")
-print("wanted to check if the update actually worked")
+print("wanted to check if the update actuallly worked")
 t = pd.read_csv("test/test_dataset.csv")
 for i in t:
     print(i)
