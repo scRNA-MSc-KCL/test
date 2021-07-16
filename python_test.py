3c2b7341dad7c0fb36f15c0cc708a73d8ff82441
scRNA-MSc-KCL <87492660+scRNA-MSc-KCL@users.noreply.github.com>
2021-07-16 13:17:44 +0100

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -1,9 +1,12 @@
-python -m pip install pandas --user
+#python -m pip install pandas --user
 import pandas as pd
 print("Testing basic print function")
 print("wanted to check if the update actuallly worked")
-t = pd.read_csv("test/test_dataset.csv")
-for i in t:
-    print(i)
+
+a = pd.DataFrame({"A":[1,2], "B":[3,4]})
+print(a)
+#t = pd.read_csv("test/test_dataset.csv")
+#for i in t:
+#    print(i)
     
 
